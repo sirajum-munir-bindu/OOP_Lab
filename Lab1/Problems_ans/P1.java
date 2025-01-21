6e8db79c7fe9837329cd4fe56d5b5b347a939d89
Sirajum Munir <162568456+sirajum-munir-bindu@users.noreply.github.com>
2025-01-21 22:01:14 +0600

    Update P1.java

--- Lab1/Problems_ans/P1.java
+++ Lab1/Problems_ans/P1.java
@@ -1,1 +1,14 @@
-a
+import java.util.*;
+
+class P1{
+	public static void main(String[] x1){
+		Scanner sc=new Scanner(System.in);
+		double f,c;
+		System.out.print("Enter F:");
+		f=sc.nextDouble();
+
+		c= ((f-32)*5)/9;
+		
+	System.out.println("Converted temperature "+f+" is = "+c);
+	}
+}
