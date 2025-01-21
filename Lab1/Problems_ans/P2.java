1cadef22e4fb5393f22fb66203e0e76fe024468c
Sirajum Munir <162568456+sirajum-munir-bindu@users.noreply.github.com>
2025-01-21 22:02:30 +0600

    Update P2.java

--- Lab1/Problems_ans/P2.java
+++ Lab1/Problems_ans/P2.java
@@ -1,1 +1,17 @@
-b
+import java.util.*;
+
+public class Problem2{
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		double P, R, SI;
+		int T;
+		System.out.print("Enter the ammont: ");
+		P = sc.nextDouble();
+		System.out.print("Enter the value:");
+		R = sc.nextDouble();
+		System.out.print("Enter the value:");
+		T = sc.nextInt();
+		SI = P*R*T/100;
+		System.out.println("Total Profit = " + SI);
+	}
+}
