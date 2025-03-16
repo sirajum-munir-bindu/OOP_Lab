f9aa9f54eeb5df40258c79551a2aef0fd91a27ce
Sirajum Munir <162568456+sirajum-munir-bindu@users.noreply.github.com>
2025-03-17 00:40:04 +0600

    Add files via upload

---
+++ Lab4/Problem_Ans_Week-4/Problem-2.java/Animal.java
@@ -0,0 +1,6 @@
+class Animal {
+    public void makeSound()
+    {
+        System.out.println("Animal Makes a sound.");
+    }
+}
---
+++ Lab4/Problem_Ans_Week-4/Problem-2.java/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal a = new Animal();
+        Animal mydog = new Dog();
+        Animal myCat = new Cat();
+
+        a.makeSound();
+        mydog.makeSound();
+        myCat.makeSound();
+    }
+}
